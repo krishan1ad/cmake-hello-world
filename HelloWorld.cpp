d68cbd6dc778062438da50e0cebc61b411794081
krishan1ad <KRISHANKUMAR1AD@GMAIL.COM>
2024-10-13 17:04:32 +0530

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,10 +1,33 @@
 #include <Speaker.h>
+#include <cstdlib>  // for rand() and srand()
+#include <ctime>    // for time()
 
 using namespace std;
 using namespace Hello;
 
 int main(int argc, char *argv[]) {
-  Speaker* speaker = new Speaker();
+    // Initialize random seed
+    srand(static_cast<unsigned int>(time(0)));
 
-  speaker->sayHello();  
+    // Array of random greetings
+    const char* greetings[] = {
+        "Hello!",
+        "Hi there!",
+        "Greetings!",
+        "Salutations!",
+        "Howdy!"
+    };
+    
+    // Select a random greeting
+    int randomIndex = rand() % (sizeof(greetings) / sizeof(greetings[0]));
+    const char* randomGreeting = greetings[randomIndex];
+
+    Speaker* speaker = new Speaker();
+
+    // Use the random greeting in some way
+    cout << randomGreeting << endl; // Print the random greeting
+    speaker->sayHello(); // Call sayHello method
+
+    delete speaker; // Clean up
+    return 0;
 }
